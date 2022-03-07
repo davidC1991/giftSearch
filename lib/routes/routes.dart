d903bee25e720c2a0117625bb72f5058ceda6952
jesus callejas <david.callejasc@hotmail.es>
2022-03-07 11:47:11 -0500

    Inicio de app giftSearch

---
+++ lib/routes/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:gif_search/ui/pages/home/home.dart';
+
+Map<String, Widget Function(BuildContext)> appRoutes = {
+'home' : ( _ )=> HomePage()
+};
